69f014bd45437ca5a305ac4dd1e8df043799c7ab
Armin <armin.bagheri.1380@gmail.com>
2016-05-17 20:01:47 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -54,7 +54,7 @@ function msg_valid(msg)
   -- Don't process outgoing messages
   if msg.out then
     print('\27[36mNot valid: msg from us\27[39m')
-    return false
+  --  return false
   end
 
   -- Before bot was started
@@ -78,7 +78,7 @@ function msg_valid(msg)
     return false
   end
 
---[[ if msg.from.id == our_id then
+ --[[ if msg.from.id == our_id then
     print('\27[36mNot valid: Msg from our id\27[39m')
     return false
   end--]]
