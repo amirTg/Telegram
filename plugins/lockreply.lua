b602b59f4cf3288d67774af11b42b830ffbb3eec
Armin <armin.bagheri.1380@gmail.com>
2016-05-18 14:21:03 +0430

    Update lockreply.lua

--- plugins/lockreply.lua
+++ plugins/lockreply.lua
@@ -6,13 +6,11 @@ local function pre_process(msg)
     local hash = 'mate:'..msg.to.id
     if redis:get(hash) and msg.reply_id and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg) then
             delete_msg(msg.id, ok_cb, true)
-            return "done"
+            return "Done"
         end
     
         return msg
     end
-
-  
 
 
 local function run(msg, matches)
